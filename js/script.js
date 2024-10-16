0ac08aaf2aefd23cfcaaebdb3ce990d8fdebfef4
michaellinsk <michael.linsk01@gmail.com>
2024-10-15 22:12:15 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// transformando o menu hamburguar em um x
+const menuHamburguer = document.querySelector('menu-hamburguer');
+menuHamburguer.addEventListener('click', () =>{
+   console.log("OK");
+});
